6bb19e3a2fe5b27995be9802ac538203688da3c4
snzxvss <camilo.sanzv@pca.edu.co>
2024-10-24 08:03:04 -0500

    firts commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   avatarUrl: string = 'assets/avatar1.jpg';
   nickname: string = '';
+  isInputFocused: boolean = false;
 
   constructor(private router: Router) {}
+
+  ngOnInit() {}
 
   onAvatarChange(newAvatarUrl: string) {
     this.avatarUrl = newAvatarUrl;
@@ -21,7 +24,15 @@ export class HomePage {
     localStorage.setItem('nickname', this.nickname);
     localStorage.setItem('avatarUrl', this.avatarUrl);
 
-    // Redirige a la página de play
+    // Redirige a la página de la sala
     this.router.navigate(['/create-room']);
+  }
+
+  onFocus() {
+    this.isInputFocused = true;
+  }
+
+  onBlur() {
+    this.isInputFocused = false;
   }
 }
